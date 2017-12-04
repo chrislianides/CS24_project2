415d7e9e379cbfba6a18f6d9cc8e1b0a9b436a91
chrislianides <clianides@umail.ucsb.edu>
2017-12-04 11:57:32 -0800

    Update list.h

--- list.h
+++ list.h
@@ -1,1 +1,36 @@
 //list.h
+//XiHao Wu and Christopher Lianides
+//Perm number: 4338109 and 8899338
+#ifndef LIST_H
+#define LIST_H
+#include "node.h"
+
+
+class list {
+
+public:
+	//constructor
+	list(string input);
+
+	//desctructor
+	~list();
+
+	//list creator
+	void create_list();
+	
+
+	//print functions
+
+	string print_infix_list();
+	string print_postfix_list();
+	string print_prefix_list();
+
+
+private:
+	Node * head;
+	Node * current;
+	string str;
+};
+
+
+#endif 
