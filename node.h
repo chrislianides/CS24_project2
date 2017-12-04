511167b2dbdf08654e93cef76a72a21004b88016
chrislianides <clianides@umail.ucsb.edu>
2017-12-04 11:52:14 -0800

    Add files via upload

---
+++ node.h
@@ -0,0 +1,25 @@
+//node.h
+#ifdef NODE_H
+#define NODE_H
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+class node{
+    Public:
+    node();
+
+
+
+    Private:
+    Node current;
+    node cursor;
+
+
+
+
+
+#endif
+
+
